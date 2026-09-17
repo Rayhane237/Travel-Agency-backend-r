5195f0e0b6bbe1ba20429e34e4016ccca11a7330
Rayhane <rayhane775@gmail.com>
2026-09-17 09:05:46 +0100

    my bookings apis

--- adminControllers/statCards.js
+++ adminControllers/statCards.js
@@ -1,21 +1,31 @@
-const BookHotel = require("../models/HotelBooking");
+const BookHotel  = require("../models/HotelBooking");
 const BookFlight = require("../models/FlightBooking");
 const User       = require("../models/User");
 const Messages   = require("../models/Messages");
+const ListHotel  = require("../models/HotelListing")
+const ListFlight = require("../models/FlightListing")
 
 const statCards = async (req, res) => {
     const totalUsers = User.countDocuments({});
     const totalMessages = Messages.countDocuments({});
     const totalHotelBookings = BookHotel.countDocuments({});
     const totalFlightBookings = BookFlight.countDocuments({});
+    const totalHotelListings  = ListHotel.countDocuments({});
+    const totalFlightListings = ListFlight.countDocuments({});
 
-    Promise.all([totalUsers, totalMessages, totalHotelBookings, totalFlightBookings])
-        .then(([usersCount, messagesCount, hotelBookingsCount, flightBookingsCount]) => {
+
+    Promise.all([totalUsers, totalMessages,
+                 totalHotelBookings, totalFlightBookings ,
+                 totalHotelListings , totalFlightListings
+    ])
+    .then(([usersCount, messagesCount, hotelBookingsCount, flightBookingsCount , hotelListingsCount ,flightListingsCount]) => {
             res.status(200).json({
                 totalUsers: usersCount,
                 totalMessages: messagesCount,
                 totalHotelBookings: hotelBookingsCount,
-                totalFlightBookings: flightBookingsCount
+                totalFlightBookings: flightBookingsCount,
+                totalHotelListings: hotelListingsCount,
+                totalFlightListings: flightListingsCount,
             });
         })
         .catch(err => {
